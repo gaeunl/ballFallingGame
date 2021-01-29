691f9f8196591b3f85c7e3ceeff3ad110e6ebbe2
Gaeun Lee <gaeun1065@gmail.com>
2021-01-29 10:38:05 -0800

    create a ball

---
+++ script.js
@@ -0,0 +1,35 @@
+var character = document.getElementById("character");
+var interval;
+var both = 0;
+
+function moveLeft(){
+    var left = parseInt(window.getComputedStyle(character).getPropertyValue('left'));
+    if(left > 0){
+        character.style.left = left - 2 + 'px';
+    }
+}
+
+function moveRight(){
+    var left = parseInt(window.getComputedStyle(character).getPropertyValue('left'));
+    if(left < 380){ // width - characterwidth
+        character.style.left = left + 2 + 'px';
+    }
+}
+
+document.addEventListener('keydown', event => {
+    if(both !== 0){
+        return;
+    }
+    both ++;
+    if(event.key === "ArrowLeft"){
+        interval = setInterval(moveLeft, 1);
+    }
+    if (event.key === "ArrowRight"){
+        interval = setInterval(moveRight, 1);
+    }
+})
+
+document.addEventListener('keyup', event => {
+    clearInterval(interval);
+    both = 0;
+})
